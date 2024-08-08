2a4784f7aafc14d1eb21397adaa9adaf57efea07
Bryant <139320860+Buraito-stack@users.noreply.github.com>
2024-08-08 07:37:08 +0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,5 +1,6 @@
 -- 1. List all transactions
-SELECT * FROM transactions;
+SELECT * 
+FROM transactions;
 
 -- 2. List all transactions with columns: transactions.id, products.name, categories.name
 SELECT 
@@ -13,7 +14,7 @@ INNER JOIN
 INNER JOIN
     categories ON products.category_id = categories.id;
 
--- 3 List only 10 transactions, starting from record no 21
+-- 3. List only 10 transactions, starting from record no 21
 SELECT 
     transactions.id,
     transactions.product_id
@@ -23,38 +24,55 @@ ORDER BY
     transactions.id
 LIMIT 10 OFFSET 20;
 
--- 4. List transaction only for 'Radio'
+-- 4. List transactions only for 'Radio'
 SELECT *
-FROM transactions
-WHERE product_name ='Radio';
-
--- 5. List transaction only for 'Radio' between '2024-01-01' and '2024-02-15'
-SELECT *
-FROM transactions
-WHERE product_name ='Radio'
-AND created_at between '2024-01-01' AND '2024-02-15';
-
--- 6. List transaction for 'Microwave' in February 2024
-SELECT *
-FROM transactions
-WHERE product_name ='Microwave'
-AND EXTRACT(MONTH FROM created_at) = 2;
-
--- 7. Sum transaction from Microwave in Feb 2024
-SELECT 
-SUM(total) AS total_sum
 FROM 
-transactions 
-WHERE product_name = 'Microwave' 
-AND EXTRACT(MONTH FROM created_at) = 2;
+    transactions
+WHERE 
+    product_name = 'Radio';
 
--- 8. List transaction from TV only in 2024
+-- 5. List transactions only for 'Radio' between '2024-01-01' and '2024-02-15'
 SELECT *
-FROM transactions
-WHERE product_name = 'TV'
-AND EXTRACT(YEAR FROM created_at) = 2024;
+FROM 
+    transactions
+WHERE 
+    product_name = 'Radio'
+AND 
+    created_at BETWEEN '2024-01-01' AND '2024-02-15';
 
--- 9. Count number of transactions that TV has for each year
+-- 6. List transactions for 'Microwave' in February 2024
+SELECT *
+FROM 
+    transactions
+WHERE 
+    product_name = 'Microwave'
+AND 
+    EXTRACT(MONTH FROM created_at) = 2
+AND 
+    EXTRACT(YEAR FROM created_at) = 2024;
+
+-- 7. Sum transactions from 'Microwave' in February 2024
+SELECT 
+    SUM(total) AS total_sum
+FROM 
+    transactions 
+WHERE 
+    product_name = 'Microwave' 
+AND 
+    EXTRACT(MONTH FROM created_at) = 2
+AND 
+    EXTRACT(YEAR FROM created_at) = 2024;
+
+-- 8. List transactions from 'TV' only in 2024
+SELECT *
+FROM 
+    transactions
+WHERE 
+    product_name = 'TV'
+AND 
+    EXTRACT(YEAR FROM created_at) = 2024;
+
+-- 9. Count the number of transactions that 'TV' has for each year
 SELECT 
     YEAR(created_at) AS year,
     COUNT(*) AS transaction_count
@@ -65,17 +83,17 @@ WHERE
 GROUP BY 
     YEAR(created_at);
 
--- 10. Sum the qty from all transactions for each products 
+-- 10. Sum the qty from all transactions for each product
 SELECT
-product_name,
-SUM(qty) AS total_qty
-FROM transactions
-GROUP BY product_name;
+    product_name,
+    SUM(qty) AS total_qty
+FROM 
+    transactions
+GROUP BY 
+    product_name;
 
--- 11. Find avg total for all transactions
+-- 11. Find the average total for all transactions
 SELECT
-AVG(total) AS avg_total
-FROM transactions;
-
-
-
+    AVG(total) AS avg_total
+FROM 
+    transactions;
